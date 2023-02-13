6020436f46ca30f9147f6df30450fe3a44322969
Patrick Eads <peads@users.noreply.github.com>
2023-02-13 14:52:40 +0000

    Incremental

--- complex/com.c
+++ complex/com.c
@@ -25,20 +25,28 @@
 #define TIMING_RUNS 4
 #include <assert.h>
 #include "timed_functions.h"
+
+#define MAX_ITERATIONS 128
+#define MAX_ERROR 1e-12
+
 uint8_t DEBUG = 0;
 
 struct complexArgs {
-    int ar;
-    int aj;
-    int br;
-    int bj;
+    double ar;
+    double aj;
+    double br;
+    double bj;
 };
 
 union vect {
     __m256d vect;
 };
 
-static double norm(int x, int y) {
+const char *runNames[TIMING_RUNS]
+        = {"calcAtan2 :: ", "calcAsin :: ", 
+            "calcVect :: ", "caclVectPd :: "};//, "esbensen :: "};
+
+static double norm(double x, double y) {
 
     return sqrt(x * x + y * y);
 }
@@ -98,12 +106,12 @@ static void calcVect(void *args, double *result) {
     *result = asin(zr / r);
 }
 
-static void calcAcos(void *args, double *result) {
+static void calcAsin(void *args, double *result) {
 
     struct complexArgs *cargs = args;
 
-    int vr = cargs->ar * cargs->br - cargs->aj * cargs->bj;
-    int vj = cargs->aj * cargs->br + cargs->ar * cargs->bj;
+    double vr = cargs->ar * cargs->br - cargs->aj * cargs->bj;
+    double vj = cargs->aj * cargs->br + cargs->ar * cargs->bj;
     double vnorm = norm(vr, vj);
 
     *result = asin(vr / vnorm);
@@ -113,18 +121,56 @@ static void calcAtan2(void *args, double *result) {
 
     struct complexArgs *cargs = args;
 
-    int vr = cargs->ar * cargs->br - cargs->aj * cargs->bj;
-    int vj = cargs->aj * cargs->br + cargs->ar * cargs->bj;
+    double vr = cargs->ar * cargs->br - cargs->aj * cargs->bj;
+    double vj = cargs->aj * cargs->br + cargs->ar * cargs->bj;
 
     *result = atan2((double) vr, (double) vj);
 }
 
-static void mult(int ar, int aj, int br, int bj/*, int *vr, int *vj*/) {
+void esbensen(void *args, double *result)
+/*
+  input signal: s(t) = a*exp(-i*w*t+p)
+  a = amplitude, w = angular freq, p = phase difference
+  solve w
+  s' = -i(w)*a*exp(-i*w*t+p)
+  s'*conj(s) = -i*w*a*a
+  s'*conj(s) / |s|^2 = -i*w
+*/
+{
 
-    static long double maxAvgErr = -1.0;
+    const int32_t scaled_pi = 2608;// 1<<14 / (2*pi) */
+    struct complexArgs *cargs = args;
+
+    int cj, dr, dj;
+    int br = (int) cargs->br;
+    int ar = (int) cargs->ar;
+    int bj = (int) cargs->bj;
+    int aj = (int) cargs->aj;
+
+    dr = (br - ar) << 1;
+    dj = (bj - aj) << 1;
+    cj = bj * dr - br * dj; /* imag(ds*conj(s)) */
+
+    int32_t denom = ar*ar + aj*aj + 1;
+    printf("dr = %d dj = %d cj = %d dividing %d * %d / %d\n", dr, dj, cj, scaled_pi, cj, denom);
+    double d = (double) cj / denom;
+
+    *result = sin(scaled_pi * d) / 2.0;
+}
+
+struct {
+    long double maxAvgErr;
+    char *name;
+} errorInfo;
+
+uint64_t totalErrors = 0;
+static void mult(double ar, double aj, double br, double bj/*, int *vr, int *vj*/) {
+
     long double avgError;
+    long double localMax; 
     struct complexArgs cargs;
     double results[TIMING_RUNS];
+    uint32_t i, localMaxIdx;
 
     cargs.aj = aj;
     cargs.ar = ar;
@@ -132,34 +178,51 @@ static void mult(int ar, int aj, int br, int bj/*, int *vr, int *vj*/) {
     cargs.bj = bj;
 
     timeFun((timedFun) calcAtan2, &cargs, (void *) &results, 0);
-    timeFun((timedFun) calcAcos, &cargs, (void *)&results, 1);
+    timeFun((timedFun) calcAsin, &cargs, (void *)&results, 1);
     timeFun((timedFun) calcVect, &cargs, (void *)&results, 2);
     timeFun((timedFun) calcVectPd, &cargs, (void *)&results, 3);
+//    timeFun((timedFun) esbensen, &cargs, (void *)&results, 4);
 
-    long double errs[4] = {results[0]-results[1],
-                           results[0]-results[2],
-                           results[0]-results[3]};
+    long double absRes = fabs(results[0]);
+    long double errs[3] = {absRes-fabs(results[1]),
+                           absRes-fabs(results[2]),
+                           absRes-fabs(results[3]),
+                           };//results[0]-results[4]};
 
-    if (DEBUG) {
-        avgError = errs[0];
-        avgError *= errs[1];
-        avgError *= errs[2];
-        avgError = cbrtl(avgError);
+    avgError = errs[0];
+    avgError *= errs[1];
+    avgError *= errs[2];
+    avgError = cbrtl(avgError);
 //    avgError += errs[1];
 //    avgError += errs[2];
 //    avgError /= 4.0;
-        maxAvgErr = avgError > maxAvgErr ? avgError : maxAvgErr;
+      //  maxAvgErr = avgError > maxAvgErr ? avgError : maxAvgErr;
+    if (avgError > errorInfo.maxAvgErr) {
+        errorInfo.maxAvgErr = avgError;
+        for (localMaxIdx = -1, localMax = -1.0, i = 0; i < 3; ++i) {
+            if (errs[i] > localMax) {
+                localMax = errs[i];
+                errorInfo.name = runNames[i+1];
+            }
+        }
+    }
 
-        printf("(%d + %di) . (%d + %di) = (%d + %di) angle: %f, %f, %f, %f "
-               "max avg error: %Le\n",
+    if (DEBUG 
+        || errs[0] != 0.0 && errs[0] >= MAX_ERROR 
+        || errs[1] != 0.0 && errs[1] >= MAX_ERROR 
+        || errs[2] != 0.0 && errs[2] >= MAX_ERROR) {
+        printf("(%f + %fi) . (%f + %fi) = (%f + %fi) angle: %f, %f, %f, %f "
+               "err1: %Le, err2: %Le, err3: %Le cause: %s\n",
                ar, aj,
                br, bj,
                ar * br - aj * bj,
                aj * br + ar * bj,
-               results[0], results[1], results[2], results[3],
-               maxAvgErr);
+               results[0], results[1], results[2], results[3], //results[4],
+               errs[0], errs[1], errs[2], errorInfo.name);
+        totalErrors++;
     }
-    assert(errs[0] < 1e-14 && errs[1] < 1e-14 && errs[2] < 1e-14);
+
+//    assert(errs[0] < MAX_ERROR && errs[1] < MAX_ERROR && errs[2] < MAX_ERROR);
 }
 
 int main(const int argc, const char **argv) {
@@ -167,21 +230,27 @@ int main(const int argc, const char **argv) {
     if (argc > 1) {
         DEBUG = 'v' == argv[1][0];
     }
-
-    int i,j,k,m;
-    for (i = 1; i < 32; ++i)
-        for (j = 1; j < 32; ++j)
-            for (k = 1; k < 32; ++k)
-                for (m = 1; m < 32; ++m)
-                    mult(i,j,k,m);
+    errorInfo.maxAvgErr = -1.0;
+    uint32_t i,j,k,m;
+    double f,n,p,q;
+    for (i = 1, f = -10.0; i < MAX_ITERATIONS; ++i, f += 0.01) {
+        for (j = 1, n = -10.0; j < MAX_ITERATIONS; ++j, n += 0.01) {
+            for (k = 1, p = -10.0; k < MAX_ITERATIONS; ++k, p += 0.01) {
+                for (m = 1, q = -10.0; m < MAX_ITERATIONS; ++m, p += 0.01) {
+                    if (0.0 == f || 0.0 == n) continue;
+                    //mult(i,j,k,m);
+                    mult(f,n,p,q);
+                }
+            }
+        }
+    }
 
     mult((int16_t) 1, (int16_t) 2, (int16_t) 3, (int16_t) 4);
     mult(5, 6, 7, 8);
     mult(9, 10, 11, 12);
 
-    char *runNames[TIMING_RUNS]
-            = {"calcAtan2 :: ", "calcAcos :: ", "calcVect :: ", "caclVectPd :: "};
     printTimedRuns(runNames, TIMING_RUNS);
-
+    printf("Worst: %s, Max avg error: %Le, total above threshold: %lu, total calculations: %lu\n", 
+        errorInfo.name, errorInfo.maxAvgErr, totalErrors, MAX_ITERATIONS*MAX_ITERATIONS*MAX_ITERATIONS*MAX_ITERATIONS);
     return 0;
 }
