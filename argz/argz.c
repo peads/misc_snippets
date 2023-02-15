6cc9d9474a18fc54cf452b4db3c9c6e18a735e73
Patrick Eads <peads@users.noreply.github.com>
2023-02-15 16:33:07 -0500

    Testing assertions etc.

--- argz/argz.c
+++ argz/argz.c
@@ -15,41 +15,25 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define TIMING_RUNS 3
-
+#define TIMING_RUNS 4
+#include <immintrin.h>
 #include "timed_functions.h"
 
 #define MIN -2
 #define MAX 2
 #define STEP 1
 #define MAX_ERROR 1e-1
-#define ARGS 4
+#ifdef DEBUG
+    #define ASSERT(b) assert(b)
+#else
+    #define ASSERT(b) //
+#endif
 
-#define NIBBLE_TO_BINARY_PATTERN "%c%c%c%c"
-#define BYTE_TO_BINARY_PATTERN NIBBLE_TO_BINARY_PATTERN NIBBLE_TO_BINARY_PATTERN
+char *runNames[TIMING_RUNS] = {"vect1 :: ", "polar_discriminant :: ", "polar_disc_fast :: ",
+                               "esbensen :: " };
 
-#define NIBBLE_TO_BINARY(b)  \
-  (b & 0x08 ? '1' : '0'), \
-  (b & 0x04 ? '1' : '0'), \
-  (b & 0x02 ? '1' : '0'), \
-  (b & 0x01 ? '1' : '0')
-
-#define BYTE_TO_BINARY(b) \
-  (b & 0x80 ? '1' : '0'), \
-  (b & 0x40 ? '1' : '0'), \
-  (b & 0x20 ? '1' : '0'), \
-  (b & 0x10 ? '1' : '0'), \
-  (b & 0x08 ? '1' : '0'), \
-  (b & 0x04 ? '1' : '0'), \
-  (b & 0x02 ? '1' : '0'), \
-  (b & 0x01 ? '1' : '0')
-
-static const uint64_t SQUARE_SIDE = (MAX - MIN) / STEP + 1;
-static const uint8_t BIT_MASK = (1 << ARGS) - 1;
-
-char *runNames[TIMING_RUNS] = {"polar_discriminant :: ", "polar_disc_fast :: ", "esbensen :: "};
-
-typedef int (*argzFun)(int a, int b, int c, int d);
+//typedef int (*argzFun)(int a, int b, int c, int d);
+typedef double (*argzFun)(int a, int b, int c, int d);
 
 struct argzArgs {
     int ar;
@@ -59,48 +43,48 @@ struct argzArgs {
     argzFun fun;
 };
 
-extern int bsr(uint64_t i);
-__asm__ (
-#ifdef __APPLE_CC__
-"_bsr: "
-#else
-"bsr: "
-#endif
-    "movq $-1023, %rdx\n\t" // store 0 to compare against ZF later
-    "bsr %rdi, %rax\n\t"
-    "cmovzq %rdx, %rax\n\t"
-    "ret"
-);
+//extern int bsr(uint64_t i);
+//__asm__ (
+//#ifdef __APPLE_CC__
+//"_bsr: "
+//#else
+//"bsr: "
+//#endif
+//    "movq $-1023, %rdx\n\t" // store 0 to compare against ZF later
+//    "bsr %rdi, %rax\n\t"
+//    "cmovzq %rdx, %rax\n\t"
+//    "ret"
+//);
+//
+//extern uint64_t lzcnt(uint64_t i);
+//__asm__ (
+//#ifdef __APPLE_CC__
+//"_lzcnt: "
+//    #else
+//"lzcnt: "
+//    #endif
+//    "movq $0, %rdx\n\t" // store 0 to return if CF set later
+//    "lzcnt %rdi, %rax\n\t"
+//    "cmovcq %rdx, %rax\n\t"
+//    "ret"
+//);
 
-extern uint64_t lzcnt(uint64_t i);
-__asm__ (
-#ifdef __APPLE_CC__
-"_lzcnt: "
-    #else
-"lzcnt: "
-    #endif
-    "movq $0, %rdx\n\t" // store 0 to return if CF set later
-    "lzcnt %rdi, %rax\n\t"
-    "cmovcq %rdx, %rax\n\t"
-    "ret"
-);
-
-extern int bsrLzcnt(uint64_t i);
-__asm__ (
-#ifdef __APPLE_CC__
-"_bsrLzcnt: "
-    #else
-"bsrLzcnt: "
-    #endif
-    "lzcnt %rdi, %rdx\n\t"
-    "jc zero\n\t" // jump to return -1023 if CF set
-    "movq $63, %rax\n\t"
-    "subq %rdx, %rax\n\t"
-    "ret\n\t"
-"zero:\n\t"
-    "movq $-1023, %rax\n\t"
-    "ret"
-);
+//extern int bsrLzcnt(uint64_t i);
+//__asm__ (
+//#ifdef __APPLE_CC__
+//"_bsrLzcnt: "
+//    #else
+//"bsrLzcnt: "
+//    #endif
+//    "lzcnt %rdi, %rdx\n\t"
+//    "jc zero\n\t" // jump to return -1023 if input was 0
+//    "movq $63, %rax\n\t"
+//    "subq %rdx, %rax\n\t"
+//    "ret\n\t"
+//"zero:\n\t"
+//    "movq $-1023, %rax\n\t"
+//    "ret"
+//);
 
 static inline void multiply(int ar, int aj, int br, int bj, int *cr, int *cj) {
 
@@ -108,12 +92,12 @@ static inline void multiply(int ar, int aj, int br, int bj, int *cr, int *cj) {
     *cj = aj * br + ar * bj;
 }
 
-static int fast_atan2(int y, int x)
+static double fast_atan2(double y, double x)
 /* pre scaled for int16 */
 {
 
-    int yabs, angle;
-    int pi4 = (1 << 12), pi34 = 3 * (1 << 12);  /* note pi = 1<<14 */
+    double yabs, angle;
+//    int pi4 = (1 << 12), pi34 = 3 * (1 << 12);  /* note pi = 1<<14 */
     if (x == 0 && y == 0) {
         return 0;
     }
@@ -122,33 +106,31 @@ static int fast_atan2(int y, int x)
         yabs = -yabs;
     }
     if (x >= 0) {
-        angle = pi4 - pi4 * (x - yabs) / (x + yabs);
+        angle = M_PI_2 - (x - yabs) / (x + yabs) + 1.0;
     } else {
-        angle = pi34 - pi4 * (x + yabs) / (yabs - x);
+        angle = (y < 0.0 ? M_PI_2 : -M_PI_2) - (x + yabs) / (yabs - x) + 1.0;
     }
-    if (y < 0) {
+    if (y < 0.0) {
         return -angle;
     }
     return angle;
 }
 
-static int polar_disc_fast(int ar, int aj, int br, int bj) {
+static double polar_disc_fast(int ar, int aj, int br, int bj) {
 
     int cr, cj;
     multiply(ar, aj, br, -bj, &cr, &cj);
     return fast_atan2(cj, cr);
 }
 
-static int polar_discriminant(int ar, int aj, int br, int bj) {
+static double polar_discriminant(int ar, int aj, int br, int bj) {
 
     int cr, cj;
-    double angle;
-    multiply(ar, aj, br, -bj, &cr, &cj);
-    angle = atan2((double) cj, (double) cr);
-    return (int) (angle / 3.14159 * (1 << 14));
+    multiply(ar, aj, br, bj, &cr, &cj);
+    return atan2((double) cj, (double) cr);
 }
 
-static int esbensen(int ar, int aj, int br, int bj)
+static double esbensen(int ar, int aj, int br, int bj)
 /*
   input signal: s(t) = a*exp(-i*w*t+p)
   a = amplitude, w = angular freq, p = phase difference
@@ -167,95 +149,126 @@ static int esbensen(int ar, int aj, int br, int bj)
     return (scaled_pi * cj / (ar * ar + aj * aj + 1));
 }
 
-static int checkBounds(int *ar, int *aj) {
+static double calcVectPd(int ar, int aj, int br, int bj) {
 
-    if (*aj >= MAX) {
-        *ar += STEP;
-        *aj = MIN;
-        return 1;
+    union vect {
+        __m256d vect;
+    };
+
+    union vect u = {ar, aj, ar, br};
+    union vect v = {br, bj, bj, aj};                               // => ar*br, aj*bj, ar*bj, br*aj
+//    __m256d temp;
+    double zr, zj;
+
+    u.vect = _mm256_mul_pd(u.vect, v.vect);
+//    temp = _mm256_permute_pd(u.vect, 0b0101);                    // aj*bj, ar*br, br*aj, ar*bj
+    u.vect = _mm256_addsub_pd(u.vect,
+                            _mm256_permute_pd(u.vect, 0b0101)); // ar*ar - aj*bj, aj*bj + ar*br, ar*bj - br*aj, br*aj + ar*bj
+    zr = u.vect[0];                                                // ar*ar - aj*bj
+    zj = u.vect[3];                                                // br*aj + ar*bj
+    if (0.0 == zr && 0.0 == zj) {
+        return NAN;
     }
-    return 0;
+
+    u.vect = _mm256_mul_pd(u.vect, u.vect);                  // (ar*ar - aj*bj)^2, (aj*aj + ar*br)^2, (ar*bj - br*aj)^2, (br*aj + ar*bj)^2
+//    temp = _mm256_permute_pd(u.vect, _MM_SHUFFLE(3, 2, 1, 0));   // (ar*ar - aj*bj)^2, (ar*ar - aj*bj)^2, (br*aj + ar*bj)^2, (ar*bj - br*aj)^2
+//    v.vect = _mm256_permute2f128_pd(u.vect, u.vect, 1);          // (ar*bj - br*aj)^2, (br*aj + ar*bj)^2, (ar*ar - aj*bj)^2, (ar*bj - br*aj)^2
+//    u.vect = _mm256_add_pd(temp, v.vect);                        // u[0] + u[2], u[0] + u[3], u[3] + u[0], u[2] + u[1]
+    u.vect = _mm256_permute_pd(u.vect, _MM_SHUFFLE(3, 2, 1, 0));
+    u.vect = _mm256_add_pd(u.vect, _mm256_permute2f128_pd(u.vect, u.vect, 1));
+    u.vect = _mm256_sqrt_pd(u.vect);                            // (u[0] + u[2])^1/2, (u[0] + u[3])^1/2, (u[3] + u[0])^1/2, (u[2] + u[1])^1/2
+
+#ifndef DEBUG
+    return acos(zr / u.vect[0]);
+#else
+    static uint64_t errCnt = 0;
+    double result =  acos(zr / u.vect[0]);                          // ((ar*ar - aj*bj)^2 + (br*aj + ar*bj)^2)^1/2
+
+    int mulr = ar * br - aj * bj;
+    int mulj = ar * bj + br * aj;
+    ASSERT(mulr == zr);
+    ASSERT(mulj == zj);
+//    multiply(ar, aj, br, bj, &mulr, &mulj);
+    double phase = atan2(mulj, mulr);
+
+    long double delta = fabsl(fabsl(result) - fabsl(phase));
+    int isWrong = delta >= 1e-15L;
+    if (isWrong) {
+        printf("%s a := (%d + %di), b := (%d + %di)\n", runNames[3], ar, aj, br, bj);
+        printf("%s a := (%d + %di), b := (%d + %di)\n", runNames[3], ar, aj, br, bj);
+        printf("%llu - Expected phase: %f Got phase: %f Delta: %Lf\n", errCnt++, phase, result, delta);
+    }
+    ASSERT(!isWrong);
+
+    return result;
+#endif
 }
 
-typedef int (*bitsFun)(uint64_t i);
-struct bitsArgs {
-    bitsFun fun;
-    uint64_t i;
-};
+void runTest(void *arg, double *result) {
 
-void runTest(void *arg, uint64_t *result) {
-    struct bitsArgs *bargs = arg;
+    struct argzArgs *bargs = arg;
 
-    *result = bargs->fun(bargs->i);
+    *result = bargs->fun(bargs->ar, bargs->aj, bargs->br, bargs->bj);
 }
 
-void testIteration(struct bitsArgs *args, void *results, int runIndex) {
+void testIteration(struct argzArgs *args, void *results, int runIndex) {
 
-    int trueValue = bitScanReverse(args->i);
+    double mulr = args->ar * args->br - args->aj * args->bj;
+    double mulj = args->ar * args->bj + args->br * args->aj;
+    double phase = atan2(mulj, mulr);
 
     timeFun((timedFun) runTest, args, results, runIndex);
 
-    int result = ((uint64_t *)results)[runIndex];
+    double result = ((double *) results)[runIndex];
 
-    printf("bitScanReverse: %d testFun: %d\n", trueValue, result);
-    assert(trueValue == result);
+    long double delta = fabsl(fabsl(result) - fabsl(phase));
+    int isWrong = delta >= 1e-15L;
+    if (isWrong) {
+        printf("%-25s a := (%d + %di), b := (%d +%di), phase := %f\n",
+               runNames[runIndex], args->ar, args->aj, args->br, args->bj, result);
+        printf("Expected phase: %f\n", phase);
+    }
 }
 
 int main(void) {
 
-    static const uint64_t n = SQUARE_SIDE*SQUARE_SIDE*SQUARE_SIDE*SQUARE_SIDE;
-
-    int i;
+    int m, k, j, i;
     struct argzArgs args;
+    double results[TIMING_RUNS];
 
-//    for (   i = 0, args.bj = args.br = args.ar = args.aj = MIN;
-//            i > n;
-//            ++i, ++args.bj) {
-//
-//        if ((i & BIT_MASK) >> 1) {
-//            ++args.br;
-//        }
-//        if ((i & BIT_MASK) >> 2) {
-//            ++args.ar;
-//        }
-//        if ((i & BIT_MASK) >> 3) {
-//            ++args.ar;
-//        }
-//
-//        printf("%d %d %d %d\n", args.ar, args.aj, args.br, args.bj);
-////        printf(NIBBLE_TO_BINARY_PATTERN"\n", NIBBLE_TO_BINARY(i & BIT_MASK));
-//
-//    }
+    args.bj = args.br = args.ar = args.aj = MIN;
+    args.ar = 1;
+    args.br = 3;
+    args.aj = 2;
+    args.fun = calcVectPd;
+    args.bj = 4;
+    testIteration(&args, results, 0);
 
-    uint64_t results[2];
-    struct bitsArgs bargs;
-    int j;
-    for (j = 0; j < 16; ++j) {
-        bargs.i = j;
+    for (i = MIN; i < MAX; ++i) {
+        args.ar = i;
+        for (j = MIN; j < MAX; ++j) {
+            args.aj = j;
+            for (k = MIN; k < MAX; ++k) {
+                args.br = k;
+                for (m = MIN; m < MAX; ++m) {
+                    args.bj = m;
 
-        bargs.fun = bsrLzcnt;
-        testIteration(&bargs, (void *) &results, 0);
 
-        bargs.fun = bsr;
-        testIteration(&bargs, (void *) &results, 1);
+                    args.fun = calcVectPd;
+                    testIteration(&args, results, 0);
+
+                    args.fun = polar_discriminant;
+                    testIteration(&args, results, 1);
+
+                    args.fun = polar_disc_fast;
+                    testIteration(&args, results, 2);
+
+                    args.fun = esbensen;
+                    testIteration(&args, results, 3);
+                }
+            }
+        }
     }
 
-    char *runNames[2] = {"bsrLzcnt :: ", "bsr :: "};
-    printTimedRuns(runNames, 2);
-
-//    uint64_t testVal = 0x000f0000;
-//    uint64_t ret = findMsb(testVal);
-//    printf("bitScanReverse: 0x%X bsr: 0x%llX lzcntBsr: 0x%llX findMsb: 0x%llX 0b"
-//            BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN
-//            BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN"\n",
-//
-//           bitScanReverse(testVal), bsr(testVal), bsrLzcnt(testVal), ret,
-//           BYTE_TO_BINARY(ret >> 0x38),
-//           BYTE_TO_BINARY(ret >> 0x30),
-//           BYTE_TO_BINARY(ret >> 0x28),
-//           BYTE_TO_BINARY(ret >> 0x20),
-//           BYTE_TO_BINARY(ret >> 0x18),
-//           BYTE_TO_BINARY(ret >> 0x10),
-//           BYTE_TO_BINARY(ret >> 0x8),
-//           BYTE_TO_BINARY(ret));
+    printTimedRuns(runNames, 4);
 }
