4dce475093391074f3cf1f256999d2de9beff7c9
Patrick Eads <peads@users.noreply.github.com>
2023-02-25 18:04:07 -0500

    Incremenetal

--- breakinguparrays/breakinguparrays.c
+++ breakinguparrays/breakinguparrays.c
@@ -18,37 +18,35 @@
 #include <string.h>
 #include "timed_functions.h"
 
-#define SHIFT_SIZE 5
+#define LOG2_LENGTH 4
+#define LENGTH (1 << LOG2_LENGTH)
 
 union m256_8 {
-    uint8_t buf[32] /*__attribute__((aligned(32))*/;
+    uint8_t buf[LENGTH] /*__attribute__((aligned(32))*/;
     __m256i v;
-    int16_t buf16[16];
+    int16_t buf16[LENGTH];
 
 };
 
-//union m256_16 {
-//    __m256i v;
-//    int16_t buf[16];
-//};
+int isCheckADCMax = 1;
 
-//    static const __m256i zero = {0,0,0,0};
+static const __m256i zero = {0,0,0,0};
 static const __m256i Z // all 127s
     = {0x7f7f7f7f7f7f7f7f, 0x7f7f7f7f7f7f7f7f, 0x7f7f7f7f7f7f7f7f, 0x7f7f7f7f7f7f7f7f};
+static const __m256i W // all 127s
+        = {0xffffffffffffffff, 0xffffffffffffffff,0xffffffffffffffff,0xffffffffffffffff};
 
 static __m256i *buf8;
-//static __m256i *buf16;
 
-int isCheckADCMax = 1;
 
 static inline uint8_t uCharMax(uint8_t a, uint8_t b) {
     return a > b ? a : b;
 }
 
-static inline __m256i convert(__m256i data) {
+static inline __m128i convert(__m256i data) {
     __m128i lo_lane = _mm256_castsi256_si128(data);
     __m128i hi_lane = _mm256_extracti128_si256(data, 1);
-    return _mm256_castsi128_si256(_mm_packus_epi16(lo_lane, hi_lane));
+    return _mm_packus_epi16(lo_lane, hi_lane);
 }
 
 static void convertTo16Bit(uint32_t len) {
@@ -76,8 +74,8 @@ static void convertTo16Bit(uint32_t len) {
 
     for (i = 0; i < len; i++) {
         buf8[i] = _mm256_sub_epi8(buf8[i], Z);
+//        buf8[i] = _mm256_packs_epi16(_mm256_unpackhi_epi8(buf8[i], zero), _mm256_unpacklo_epi8(buf8[i], zero));
     }
-
     if (isCheckADCMax) {
         for (i = 0; i < len; i+=2) {
 
@@ -94,12 +92,11 @@ static void breakit(uint8_t *arr, const uint32_t len, const uint32_t size) {
     int i;
     uint32_t unit = sizeof(uint8_t);
     long leftToProcess = len;
-    uint32_t step = unit << SHIFT_SIZE;
+    uint32_t step = unit << LOG2_LENGTH;
     uint32_t chunk = step * unit;
     union m256_8 z;
 
     buf8 = calloc(size * chunk, sizeof(__m256i));
-//    buf16 = calloc(size * chunk, sizeof(__m256i));
 
     for (i = 0; leftToProcess > 0; i += step, ++j) {
 
@@ -111,15 +108,12 @@ static void breakit(uint8_t *arr, const uint32_t len, const uint32_t size) {
 
 int main(void) {
 
-    printf("sizeof :: short %lu, int: %lu, long: %lu, uint8_t: %lu, uint16_t: %lu, uint32_t: %lu\n",
-           sizeof(char), sizeof(short), sizeof(int), sizeof(uint8_t), sizeof(uint16_t), sizeof(uint32_t));
-
     srand(time(NULL));
 
     __m256i tmp;
     uint8_t arr[(1 << 6) + 27] /*__attribute__((aligned(32))*/;
     uint32_t len = sizeof(arr) / sizeof(*arr);
-    uint32_t size = len / (sizeof(char) << SHIFT_SIZE) + 1;
+    uint32_t size = len / (sizeof(char) << LOG2_LENGTH) + 1;
     uint8_t val /*__attribute__((aligned(32))*/;
     uint16_t *temp;
     int j, i;
@@ -127,7 +121,7 @@ int main(void) {
     assert(size > 0);
 
     for (i = 0; i < len; ++i) {
-        if (i % 32 == 0) printf("\n");
+        if (i % LENGTH == 0) printf("\n");
         val = (100 + rand()) % 255;
         arr[i] = val;
         printf("%X, ", val);
@@ -138,7 +132,7 @@ int main(void) {
 
     for (i = 0; i < size; ++i) {
         union m256_8 z = {.v = buf8[i]};
-        for (j = 0; j < 32; ++j) {
+        for (j = 0; j < LENGTH; ++j) {
             printf("%X, ", z.buf[j]);
         }
         printf("\n");
@@ -151,7 +145,7 @@ int main(void) {
 
         union m256_8 z = {.v = buf8[i]};
 
-        for (j = 0; j < 32; ++j) {
+        for (j = 0; j < LENGTH; ++j) {
             printf("%X, ", z.buf[j]);
         }
         printf("\n");
@@ -162,7 +156,7 @@ int main(void) {
 
         union m256_8 z = {.v = buf8[i]};
 
-        for (j = 0; j < 32; ++j) {
+        for (j = 0; j < LENGTH; ++j) {
             printf("%X, ", z.buf16[j]);
         }
         printf("\n");
@@ -171,10 +165,10 @@ int main(void) {
 
     for (i = 0; i < size; ++i) {
 
-        temp = calloc(32, sizeof(uint16_t));
+        temp = calloc(LENGTH, sizeof(uint16_t));
         _mm256_storeu_epi16(temp, buf8[i]);
 
-        for (j = 0; j < 32; ++j) {
+        for (j = 0; j < LENGTH; ++j) {
             printf("%X, ", temp[j]);
         }
         printf("\n");
@@ -182,8 +176,8 @@ int main(void) {
     printf("\n");
 
     for (i=0; i<(int)len; i++) {
-        if (i % 32 == 0) printf("\n");
-        printf("%X, ",  (short)arr[i] - 127);
+        if (i % LENGTH == 0) printf("\n");
+        printf("%X, ",  ((short)arr[i]) - 127);
     }
     printf("\n");
 }
